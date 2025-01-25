46f6f6be9e5767f0498fd15a94f96f5d0ca3d8c6
Anurag Kumar <anuragxone@outlook.in>
2025-01-25 14:56:41 +0530

    gradle: add ktor deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,12 @@ dependencies {
         exclude(group = "com.android.support")
     }
     implementation(libs.okhttp)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.logback.classic)
 }
 
 configure<GenerateBpPluginExtension> {
